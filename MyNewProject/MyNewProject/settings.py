87b9142933deb6d323a82e535bb489f03bafd4d5
learntocodelive <70848901+learntocodelive@users.noreply.github.com>
2024-01-22 17:31:47 +0000

    added documation settings

--- MyNewProject/MyNewProject/settings.py
+++ MyNewProject/MyNewProject/settings.py
@@ -38,9 +38,30 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'drf_spectacular',
     "base"
     
 ]
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Base API Docs',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
